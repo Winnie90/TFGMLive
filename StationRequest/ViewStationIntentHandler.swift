5a1f6fa7f0960472e984c99287ae988f176c21c9
Christopher Winstanley <c.winstanley90@gmail.com>
2018-06-26 22:39:54 +0100

    Create soup intent response

--- StationRequest/ViewStationIntentHandler.swift
+++ StationRequest/ViewStationIntentHandler.swift
@@ -12,7 +12,27 @@ public class ViewStationIntentHandler: NSObject, ViewStationIntentHandling {
     
     @available(iOSApplicationExtension 12.0, *)
     public func handle(intent: ViewStationIntent, completion: @escaping (ViewStationIntentResponse) -> Void) {
-        
+        guard let ident = intent.identifier else {
+            let response = ViewStationIntentResponse(code: .failure, userActivity: nil)
+            completion(response)
+            return
+        }
+        let identifier = Int(ident) ?? 0
+        StationService.getLatestDataForStation(identifier: identifier) { (station, error) in
+            if let _ = error {
+                let response = ViewStationIntentResponse(code: .failure, userActivity: nil)
+                completion(response)
+            }
+            if let station = station {
+                if let nextTram = station.trams.first {
+                    let response = ViewStationIntentResponse.success(destination: nextTram.destination, time: nextTram.waitTime )
+                    completion(response)
+                } else {
+                    let response = ViewStationIntentResponse(code: .failure, userActivity: nil)
+                    completion(response)
+                }
+            }
+        }
     }
     
 }
