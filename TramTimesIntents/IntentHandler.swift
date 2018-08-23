8ba79c8e2cb8ed659d673b5ca4fb480b6a6798d8
Christopher Winstanley <c.winstanley90@gmail.com>
2018-08-23 22:27:12 +0100

    ViewStationIntentHandler

--- TramTimesIntents/IntentHandler.swift
+++ TramTimesIntents/IntentHandler.swift
@@ -7,14 +7,13 @@
 //
 
 import Intents
+import StationRequest
 
 class IntentHandler: INExtension {
-    
     override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
+        guard intent is ViewStationIntent else {
+            fatalError()
+        }
+        return ViewStationIntentHandler()
     }
-    
 }
