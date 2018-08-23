6f6cfe239b54417aadb77e0d821971f3af493d1c
Christopher Winstanley <c.winstanley90@gmail.com>
2018-08-23 21:29:43 +0100

    Generated Responses and Intent extension

---
+++ TramTimesIntents/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  TramTimesIntents
+//
+//  Created by Chris Winstanley on 23/08/2018.
+//  Copyright © 2018 Winstanley. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
